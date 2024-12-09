6f8289df99f9b2d38929747c1d24621cdbd1e48d
isKage <kageshu211@gmail.com>
2024-12-09 10:50:14 +0800

    not important: change some accounts password

--- web_utils/encrypt.py
+++ web_utils/encrypt.py
@@ -5,6 +5,12 @@ import uuid
 import hashlib
 from django.conf import settings
 
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mis.settings")  # 替换 your_project 为你的项目名称
+django.setup()  # 初始化 Django 环境
+
 
 def md5(string):
     """MD5加密"""
@@ -12,6 +18,12 @@ def md5(string):
     hash_object.update((string).encode('utf-8'))
     return hash_object.hexdigest()
 
+
 def uid(string):
     data = "{}-{}".format(str(uuid.uuid4()), string)
     return md5(data)
+
+
+if __name__ == '__main__':
+    pwd = 'root123456'
+    print(md5(pwd))
