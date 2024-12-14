d407e188441e05d532a26bbff4ae31671060b4a4
isKage <kageshu211@gmail.com>
2024-12-14 13:33:17 +0800

    add: admin rights

--- web/models.py
+++ web/models.py
@@ -7,6 +7,7 @@ class UserInfo(models.Model):
     username = models.CharField(verbose_name='用户名', max_length=32, db_index=True)  # db_index=True 创建索引，可以加快速度
     email = models.EmailField(verbose_name='邮箱', max_length=32)
     password = models.CharField(verbose_name='密码', max_length=32)
+    admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
